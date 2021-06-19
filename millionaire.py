ee09b4c95def9210d2efbfcd37fd22a0bb4681b4
sbrunner210 <sb6911@stern.nyu.edu>
2021-06-19 12:33:48 -0400

    Added question function

--- millionaire.py
+++ millionaire.py
@@ -5,6 +5,23 @@ test_list = list(range(1,19))
 
 level = 1
 
+# This is a sample function that we may be able to use to easily ask questions.
+def millionaire(level,question,A,B,C,D,correct_answer):
+  print(f"This is the level {level} question.")
+  print(question)
+  print(f"A. {A}")
+  print(f"B. {B}")
+  print(f"C. {C}")
+  print(f"D. {D}")
+  answer = input("What is your answer?")
+  if answer == correct_answer:
+    print("CORRECT!")
+  elif answer == "lifeline":
+    help = input("Which lifeline would you like to use?")
+    print(help)
+  else:
+    print("YOU ARE WRONG!")
+
 while True:
     index = level - 1
     answer = input(f"This is a test for question {level}: [yes, no, lifeline]")
@@ -20,3 +37,4 @@ while True:
     else:
         print("ERROR")
         break
+
