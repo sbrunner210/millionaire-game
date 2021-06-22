a4994fdb1e90d1ed7ab8da74f00a60c95c5d54dc
lancourm <al4026@stern.nyu.edu>
2021-06-22 15:39:54 -0400

    Add money input

--- millionaire.py
+++ millionaire.py
@@ -1,13 +1,20 @@
 # This is the script for the Silver Snake's "Who Wants to be a Millionaire" Game
 
 # Testing the while loop for the questions.
-test_list = list(range(1,19))
+#test_list = list(range(1,19))
 
 level = 1
 
+
+#This sets up a money list
+global money
+money = ['$100','$200','$300','$500','$1,000','$2,000','$4,000','$8,000','$16,000','$32,000','$64,000','$125,000','$250,000','$500,000','$1,000,000']
+loser_money = 
+
 # This is a sample function that we may be able to use to easily ask questions.
 def millionaire(level,question,A,B,C,D,correct_answer):
-  print(f"This is the level {level} question.")
+  #print(f"This is the level {level} question. For" {money[level-1]})
+  print('Question:', level, "for", money[level-1]) #ML edited original code to include the amount of money it is for.
   print(question)
   print(f"A. {A}")
   print(f"B. {B}")
@@ -15,7 +22,7 @@ def millionaire(level,question,A,B,C,D,correct_answer):
   print(f"D. {D}")
   answer = input("What is your answer?")
   if answer == correct_answer:
-    print("CORRECT!")
+    print("CORRECT! You just earned ", money[level], ". If you walk away now, you'll leave with ", total_money, "but if you answer the next question incorrectly, you'll leave with ", loser_money, ".")
   elif answer == "lifeline":
     help = input("Which lifeline would you like to use?")
     print(help)
