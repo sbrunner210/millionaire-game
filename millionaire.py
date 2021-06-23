b4623ee0a1f852337cf98b8158dfc1c5819f8cb9
sbrunner210 <sb6911@stern.nyu.edu>
2021-06-22 20:23:38 -0400

    Working Question Function

--- millionaire.py
+++ millionaire.py
@@ -3,8 +3,16 @@
 # Testing the while loop for the questions.
 #test_list = list(range(1,19))
 
+import random
+
 level = 1
 
+answers_dict = {
+  "a":0,
+  "b":1,
+  "c":2,
+  "d":3
+}
 
 questions = {
   "response_code":0,"results":[
@@ -31,38 +39,44 @@ def millionaire(level,question,incorrect_answers,correct_answer):
   #print(f"This is the level {level} question. For" {money[level-1]})
   print('Question#', level, "for", money[level-1]) #ML edited original code to include the amount of money it is for.
   print(question)
-  print(f"A. {A}")
-  print(f"B. {B}")
-  print(f"C. {C}")
-  print(f"D. {D}")
+  incorrect_answers.append(correct_answer)
+  random.shuffle(incorrect_answers)
+  index = incorrect_answers.index(correct_answer)
+  print(f"A. {incorrect_answers[0]}")
+  print(f"B. {incorrect_answers[1]}")
+  print(f"C. {incorrect_answers[2]}")
+  print(f"D. {incorrect_answers[3]}")
   answer = input("What is your answer?")
-  if answer == correct_answer:
-    print("CORRECT! If you walk away now, you'll leave with ", money, " but if you answer the next question incorrectly, you'll leave with ", loser_money, ".")
+  if answers_dict[answer.lower().strip()] == index:
+    print("CORRECT! If you walk away now, you'll leave with ", money[0], " but if you answer the next question incorrectly, you'll leave with ", loser_money[0], ".")
   elif answer == "lifeline":
     help = input("Which lifeline would you like to use?")
     print(help)
   else:
-    print("Ohh, too bad! You're leaving today with ", loser_money[level - 1])
+    print("Ohh, too bad! You're leaving today with ", loser_money[0])
 
-while True:
-    index = level - 1
-    print("-------------------------------------")
-    print('Question#', level, "for", money[level])
-    print(f"If you answer correctly, you'll win {money[level]}.") 
-    print (f"But if you answer incorrectly, you'll leave with {loser_money[level-1]}.")
-    print(f"If you walk away now without answering the question, you'll keep {money[level-1]}.")
-    answer = input(f"This is a test for question {level}: [yes, no, lifeline]")
-    print("-------------------------------------")
-    if answer.lower().strip() == "yes":
-        level = level + 1
-        print(f"This was the right answer. Moving onto question {level}")
-    elif answer.lower().strip() == "no":
-        print("Wrong Answer. Sorry. Game Over.")
-        break
-    elif answer.lower().strip() == "lifeline":
-        help = input("Choose a lifeline:")
-        print(f"Your selected lifeline was: {help}")
-    else:
-        print("ERROR")
-        break
+
+millionaire(1,questions["results"][0]["question"],questions["results"][0]["incorrect_answers"],questions["results"][0]["correct_answer"])
+
+# while True:
+    # index = level - 1
+    # print("-------------------------------------")
+    # print('Question#', level, "for", money[level])
+    # print(f"If you answer correctly, you'll win {money[level]}.") 
+    # print (f"But if you answer incorrectly, you'll leave with {loser_money[level-1]}.")
+    # print(f"If you walk away now without answering the question, you'll keep {money[level-1]}.")
+    # answer = input(f"This is a test for question {level}: [yes, no, lifeline]")
+    # print("-------------------------------------")
+    # if answer.lower().strip() == "yes":
+        # level = level + 1
+        # print(f"This was the right answer. Moving onto question {level}")
+    # elif answer.lower().strip() == "no":
+        # print("Wrong Answer. Sorry. Game Over.")
+        # break
+    # elif answer.lower().strip() == "lifeline":
+        # help = input("Choose a lifeline:")
+        # print(f"Your selected lifeline was: {help}")
+    # else:
+        # print("ERROR")
+        # break
 
