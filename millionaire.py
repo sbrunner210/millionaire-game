b3a1403e969938308d5bfea3355343cf2fe5469d
sbrunner210 <sb6911@stern.nyu.edu>
2021-06-23 23:06:58 -0400

    More questions and difficulty added

--- millionaire.py
+++ millionaire.py
@@ -15,7 +15,7 @@ answers_dict = {
   "d":3
 }
 
-questions = {
+easy_questions = {
   "response_code":0,"results":[
     {"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"How would one say goodbye in Spanish?","correct_answer":"Adi&oacute;s","incorrect_answers":["Hola","Au Revoir","Salir"]},
     {"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"On a dartboard, what number is directly opposite No. 1?","correct_answer":"19","incorrect_answers":["20","12","15"]},
@@ -30,13 +30,44 @@ questions = {
     ]
   }
 
+med_questions = {
+  "response_code":0,"results":[
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"What is the name of the very first video uploaded to YouTube?","correct_answer":"Me at the zoo","incorrect_answers":["tribute","carrie rides a truck","Her new puppy from great grandpa vern."]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"In 2013 how much money was lost by Nigerian scams?","correct_answer":"$12.7 Billion","incorrect_answers":["$95 Million","$956 Million","$2.7 Billion"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"Who is a co-founder of music streaming service Spotify?","correct_answer":"Daniel Ek","incorrect_answers":["Sean Parker","Felix Miller","Michael Breidenbruecker"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"Which of these is the name of a Japanese system of alternative medicine, literally meaning &quot;finger pressure&quot;?","correct_answer":"Shiatsu","incorrect_answers":["Ukiyo","Majime","Ikigai"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"What is the romanized Japanese word for &quot;university&quot;?","correct_answer":"Daigaku","incorrect_answers":["Toshokan","Jimusho","Shokudou"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"What is the name of the popular animatronic singing fish prop, singing such hits such as &quot;Don&#039;t Worry, Be Happy&quot;?","correct_answer":"Big Mouth Billy Bass","incorrect_answers":["Big Billy Bass","Singing Fish","Sardeen"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"What is a dead mall?","correct_answer":"A mall with high vacancy rates or low consumer foot traffic","incorrect_answers":["A mall with no stores","A mall that has been condemed","A mall after business hours"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"Who invented Pastafarianism?","correct_answer":"Bobby Henderson","incorrect_answers":["Eric Tignor","Bill Nye","Zach Soldi"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"What was the original name of the search engine &quot;Google&quot;?","correct_answer":"BackRub","incorrect_answers":["CatMassage","SearchPro","Netscape Navigator"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"medium","question":"Earl Grey tea is black tea flavoured with what?","correct_answer":"Bergamot oil","incorrect_answers":["Lavender","Vanilla","Honey"]}
+    ]
+  }
+
+hard_questions = {
+  "response_code":0,"results":[
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"Electronic music producer Kygo&#039;s popularity skyrocketed after a certain remix. Which song did he remix?","correct_answer":"Ed Sheeran - I See Fire","incorrect_answers":["Marvin Gaye - Sexual Healing","Coldplay - Midnight","a-ha - Take On Me"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"When was &quot;YouTube&quot; founded?","correct_answer":"February 14, 2005","incorrect_answers":["May 22, 2004","September 12, 2005","July 19, 2009"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"Sciophobia is the fear of what?","correct_answer":"Shadows","incorrect_answers":["Eating","Bright lights","Transportation"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"What is the romanized Korean word for &quot;heart&quot;?","correct_answer":"Simjang","incorrect_answers":["Aejeong","Jeongsin","Segseu"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"How many calories are in a 355 ml can of Pepsi Cola?","correct_answer":"150","incorrect_answers":["200","100","155"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"According to Fair Works Australia, how long do you have to work to get Long Service Leave?","correct_answer":"7 years","incorrect_answers":["2 years","8 years","6 months"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"Which product did Nokia, the telecommunications company, originally sell?","correct_answer":"Paper","incorrect_answers":["Phones","Computers","Processors"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"Originally another word for poppy, coquelicot is a shade of what?","correct_answer":"Red","incorrect_answers":["Green","Blue","Pink"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"Where is Apple Inc. headquartered?","correct_answer":"Cupertino, California","incorrect_answers":["Redwood City, California","Redmond, Washington","Santa Monica, CA"]},
+    {"category":"General Knowledge","type":"multiple","difficulty":"hard","question":"How many notes are there on a standard grand piano?","correct_answer":"88","incorrect_answers":["98","108","78"]}
+    ]
+  }
+
+
 #This sets up a money list
 # global money
 money = ['$0','$100','$200','$300','$500','$1,000','$2,000','$4,000','$8,000','$16,000','$32,000','$64,000','$125,000','$250,000','$500,000','$1,000,000']
 loser_money = ['$0', '$0', '$0', '$0', '$0', '$1,000', '$1,000', '$1,000', '$1,000', '$1,000', '$32,000', '$32,000', '$32,000', '$32,000', '$32,000']
 
 # Variables to randomly select a question
-quest_length = len(questions["results"])
+quest_length = len(easy_questions["results"])
 quest_pool = [*range(quest_length)]
 
 # breakpoint()
@@ -76,10 +107,13 @@ while level <= 16:
     print("-------------------------------------")
     if level >= 1 and level <= 5:
       print("EASY QUESTIONS PROMPT")
+      difficulty = easy_questions
     elif level >= 6 and level <= 10:
       print("MEDIUM QUESTIONS PROMPT")
+      difficulty = med_questions
     elif level >= 11 and level <= 15:
       print("HARD QUESTIONS PROMPT")
+      difficulty = hard_questions
     elif level == 16:
       print("CONGRATS")
       break
@@ -90,6 +124,7 @@ while level <= 16:
     print (f"But if you answer incorrectly, you'll leave with {loser_money[level-1]}.")
     print(f"If you walk away now without answering the question, you'll keep {money[level-1]}.")
     quest_num = random.choice(quest_pool)
-    millionaire(level,questions["results"][quest_num]["question"],questions["results"][quest_num]["incorrect_answers"],questions["results"][quest_num]["correct_answer"])
+    # Currently, the loop is eliminating questions from multiple pools. Needs to be fixed.
+    millionaire(level,difficulty["results"][quest_num]["question"],difficulty["results"][quest_num]["incorrect_answers"],difficulty["results"][quest_num]["correct_answer"])
     quest_pool.remove(quest_num)
     
