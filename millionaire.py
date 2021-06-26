a64e74de39b92808c2197eb574ae4ec0335c6ac8
sbrunner210 <sb6911@stern.nyu.edu>
2021-06-26 10:15:22 -0400

    Added print statements

--- millionaire.py
+++ millionaire.py
@@ -21,6 +21,17 @@ answers_dict = {
   "d":3
 }
 
+# Introductory print statements
+print("Welcome to Who Wants to Be a Millionaire! Test your knowledge for the grand prize.")
+print("This is how the game works:" + "\n")
+print("You will be asked a multiple choice question, with options A, B, C and D.")
+print("If you get the correct answer, you earn money and move onto the next question.") 
+print("But if you guess incorrectly then you lose the money you have earned,")
+print("But don't worry! We're not total monsters. There are checkpoints along the way that allow you to keep a certain amount of money should you get a question wrong further on.")
+print("The questions increase in difficulty as you progress but you can make use of a 50/50 lifeline to help you out")
+print("This will remove 2 incorrect answers.\nType Lifeline to access")
+print("One more thing, when answering, please use either A, B, C or D or you will be disqualified!")
+
 while True:
   category = input("Please enter the number for one of the following categories: \nGeneral Knowledge	9 \nEntertainment:Books	10 \nEntertainment:Film	11 \nEntertainment:Music	12 \nEntertainment:Musicals & Theatres	13 \nEntertainment:Television	14 \nEntertainment:Video Games	15 \nEntertainment:Board Games	16 \nScience & Nature	17 \nScience:Computers	18 \nScience:Mathematics	19 \nMythology	20 \nSports	21 \nGeography	22 \nHistory	23 \nPolitics	24 \nArt	25 \nCelebrities	26 \nAnimals	27 \nVehicles	28 \nEntertainment:Comics	29 \nScience:Gadgets	30 \nEntertainment:Japanese Anime & Manga	31 \nEntertainment:Cartoon & Animations	32 \n")
   print(f"You have selected category {category}")
@@ -45,7 +56,7 @@ hard_questions = json.loads(hard_response.text)
 # This is a sample function that we may be able to use to easily ask questions.
 # Rank is the input variable for the funciton. Level is the variable.
 def millionaire(rank,question,incorrect_answers,correct_answer):
-  #print(f"This is the level {level} question. For" {money[level-1]})
+  
   global level
   global lifelines
   global money
@@ -57,12 +68,12 @@ def millionaire(rank,question,incorrect_answers,correct_answer):
   all_answers.append(correct_answer)
   random.shuffle(all_answers)
   correct_index = all_answers.index(correct_answer)
-  # breakpoint()
+  
   print(html.unescape(f"A. {all_answers[0]}"))
   print(html.unescape(f"B. {all_answers[1]}"))
   print(html.unescape(f"C. {all_answers[2]}"))
   print(html.unescape(f"D. {all_answers[3]}"))
-  # print(f"The correct answer is {correct_answer}.")
+  
   answer = input("What is your answer? [Enter your answer, type 'lifeline' for help, or type 'walk' to end the game] \n").lower().strip()
   if answer not in ["a", "b", "c", "d", "walk", "lifeline"]:
     print("OOPS! Invalid input")    
@@ -76,7 +87,6 @@ def millionaire(rank,question,incorrect_answers,correct_answer):
       else:
         lifelines = lifelines - 1
         keep_index = random.randint(0,2)
-        # incorrect_answers.pop(keep_index)
         for removed_answers in incorrect_answers:
           if incorrect_answers.index(removed_answers) != keep_index:
             remove_index = all_answers.index(removed_answers)
@@ -110,16 +120,17 @@ def millionaire(rank,question,incorrect_answers,correct_answer):
 while level <= 16:
     print("-------------------------------------")
     if level >= 1 and level <= 5:
-      print("EASY QUESTIONS PROMPT")
+      print("Just started! Here's something easy for you!")
       difficulty = easy_questions
     elif level >= 6 and level <= 10:
-      print("MEDIUM QUESTIONS PROMPT")
+      print("Let's crank up the heat! Need to make you start sweating!")
       difficulty = med_questions
     elif level >= 11 and level <= 15:
-      print("HARD QUESTIONS PROMPT")
+      print("You're on a roll! Are you going to be our next millionaire?!?")
       difficulty = hard_questions
     elif level == 16:
-      print("CONGRATS")
+      print("CONGRATULATIONS!!! YOU DID IT! YOU WON A MILLION DOLLARS!\n", 
+      "Don't spend it all in one place!")
       break
     print("-------------------------------------")
     millionaire(level,
